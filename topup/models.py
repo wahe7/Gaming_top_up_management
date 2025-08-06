02bbd2857439e1f10cc1e13d7b5b875d4f7c9f6f
wahe7 <waheguru002200@gmail.com>
2025-08-07 02:58:49 +0530

    add game and topupproduct model

--- topup/models.py
+++ topup/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+  name = models.CharField(max_length=100)
+  game_id = models.CharField(max_length=50)
+  is_active = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return self.name
+  
+class TopUpProduct(models.Model):
+  game = models.ForeignKey(Game, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  in_game_currency = models.CharField(max_length=100)
+  
+  def __str__(self):
+    return f"{self.name} ({self.game.name})"     
+    
+    
