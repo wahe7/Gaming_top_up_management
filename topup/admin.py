a498776ab2758d3e880b5af468af97c464fd8023
wahe7 <waheguru002200@gmail.com>
2025-08-07 02:59:35 +0530

    register game and topupproduct

--- topup/admin.py
+++ topup/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Game, TopUpProduct
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+  list_display = ('name', 'game_id', 'is_active')
+  search_fields = ('name', 'game_id')
+  
+@admin.register(TopUpProduct)
+class TopUpProductAdmin(admin.ModelAdmin):
+  list_display = ['name', 'price', 'in_game_currency', 'game']
+  search_fields = ['name', 'game__name']
+
+
