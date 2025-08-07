aa4462685dedf3ac5b25f4b1accb68879ee6ee83
wahe7 <waheguru002200@gmail.com>
2025-08-07 15:39:40 +0530

    add dashboard url

--- topup/urls.py
+++ topup/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TopUpOrderCreateView
+from .views import TopUpOrderCreateView, analytics_dashboard
 
 urlpatterns = [
     path('api/topup/', TopUpOrderCreateView.as_view(), name='topup-order'),
+    path('dashboard/', analytics_dashboard, name='dashboard'),
 ]
