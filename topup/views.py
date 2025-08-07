7d5a24be6df1b89bcc262b93d79748ac5e687186
wahe7 <waheguru002200@gmail.com>
2025-08-07 15:20:49 +0530

    view for create topuporder

--- topup/views.py
+++ topup/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import TopUpOrderSerializer
 
-# Create your views here.
+class TopUpOrderCreateView(APIView):
+  def post(self, request):
+    serializer = TopUpOrderSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': 'Top-up order created successfully!'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
